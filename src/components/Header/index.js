df080a72d8a0b55ecb21dae633451e02137d1126
NguyenNam1102 <namnguyenlllh@gmail.com>
2023-03-17 10:13:43 +0700

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+
+const Index = () => {
+    return (
+        <h1 className="mt-6 text-cyan-900">
+            #1 Free Online Movie Streaming Site in VietNam
+        </h1>
+    )
+}
+
+export default Index
